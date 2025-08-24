dcf1c148f6dfcca978f7c4c879c830d692310f1a
vishalvsparrow <vishalvatnani@gmail.com>
2025-08-24 11:21:07 -0700

    use OLS equation for prediction and add 95% CI

--- src/howmanypizzas/models/ols_model_v1.py
+++ src/howmanypizzas/models/ols_model_v1.py
@@ -1,14 +1,26 @@
-import math
-from statsmodels.regression.linear_model import OLSResults
+#from statsmodels.regression.linear_model import OLSResults
 
-try:
-    registrations = int(input("Enter the number of registrations as of 2 days before the event: "))
+def predict_pizza_v1():
+    result = None
+    try:
+        registrations = int(input("Enter the number of registrations as of 2 days before the event: "))
+        if registrations < 0:
+            raise ValueError("Enter a non-negative number")
 
-    loaded_model = OLSResults.load("ols_model_v1.pickle")
+        #loaded_model = OLSResults.load("ols_model_v1.pickle")
+        #result = loaded_model.predict([1, registrations]).round(2).astype("float")[0]
 
-    result = loaded_model.predict([1, registrations]).round(0).astype("int")[0]
+        ## Coefficients from the OLS model in howmanypizzas/data-exploration/exploration.ipynb
+        result = int(29.7414 + 0.5560 * registrations)    
+        result_lower_bound = int(17.825820 + 0.352351 * registrations)
+        result_upper_bound = int(41.656931 + 0.759745 * registrations)
 
-    print("You can expect {} check ins based on the number of registrations. If each person eats a quarter pizza, you should order {} pizzas.".format(result, math.ceil(result/4)))
+    except ValueError as e:
+        print("Enter a valid value. {}".format(e))
 
-except ValueError:
-    print("Please enter a valid value.")
+    if result is not None:    
+        print("\nYou can expect {result:0d} check ins, with a 95% confidence interval between {result_lower_bound:0d} and {result_upper_bound:0d} check ins.".format(result=result, result_lower_bound=result_lower_bound, result_upper_bound=result_upper_bound))
+        print("\nIf each person eats 1/6th of a large pizza, then you should order {result:0d} pizzas, or between {result_lower_bound:0d} and {result_upper_bound:0d} pizzas".format(result=int(result/6), result_lower_bound=int(result_lower_bound/6), result_upper_bound=int(result_upper_bound/6)))
+
+if __name__ == '__main__':
+    predict_pizza_v1()
